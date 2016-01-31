301519b54da290a3da0af3d808a8b2eaca0be4b6
Jake Lin <jakelinau@gmail.com>
2016-01-31 16:25:20 +1100

    Add extensions.swift

---
+++ AlgorithmsPlayground.playground/Sources/extensions.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+// http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
+extension CollectionType {
+  /// Return a copy of `self` with its elements shuffled
+  func shuffle() -> [Generator.Element] {
+    var list = Array(self)
+    list.shuffleInPlace()
+    return list
+  }
+}
+
+extension MutableCollectionType where Index == Int {
+  /// Shuffle the elements of `self` in-place.
+  mutating func shuffleInPlace() {
+    // empty and single-element collections don't shuffle
+    if count < 2 { return }
+    
+    for i in 0..<count - 1 {
+      let j = Int(arc4random_uniform(UInt32(count - i))) + i
+      guard i != j else { continue }
+      swap(&self[i], &self[j])
+    }
+  }
+}
+
+extension Int {
+  public static func randomNumbers(max: Int) -> [Int] {
+    var numbers = [Int]()
+    for i in 0...max {
+      numbers.append(Int(i))
+    }
+    return numbers.shuffle()
+  }
+}
