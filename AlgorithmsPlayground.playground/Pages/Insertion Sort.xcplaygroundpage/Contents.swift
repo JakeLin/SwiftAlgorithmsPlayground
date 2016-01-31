c749c99877f5a9360bea0f1b6a949fef9822717f
Jake Lin <jakelinau@gmail.com>
2016-01-31 18:08:30 +1100

    Optimise `Insertion Sort`

--- AlgorithmsPlayground.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
+++ AlgorithmsPlayground.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
@@ -8,18 +8,18 @@ func insertionSort(var numbers: [Int]) -> [Int] {
   var iterations = 0
   var insertions = 0
   for x in 1..<numbers.count {
-    let key = numbers[x]
-    print("key=\(key)")
-    for var y = x-1; y >= 0; y -= 1 {
+    print("numbers[x]=\(numbers[x])")
+    for y in 0..<x {
       print("numbers[y]=\(numbers[y])")
-      if key < numbers[y] {
-        numbers.removeAtIndex(y+1)
-        numbers.insert(key, atIndex: y)
-        
-        insertions += 1
-      }
       iterations += 1
-      print("\(iterations): \(numbers)")
+      if numbers[x] < numbers[y] {
+        let temp = numbers[x]
+        numbers.removeAtIndex(x)
+        numbers.insert(temp, atIndex: y)
+        insertions += 1
+        print("\(insertions): \(numbers)")
+        break
+      }
     }
   }
   print("Interactions: \(iterations), insertions: \(insertions)")
